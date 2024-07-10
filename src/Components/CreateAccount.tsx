038c04a4f46085681d4cabece1aa712e4456df79
Erza0838 <muhammaderza884@gmail.com>
2024-07-11 00:42:24 +0700

    Perubahan-2

--- src/Components/CreateAccount.tsx
+++ src/Components/CreateAccount.tsx
@@ -1,8 +1,9 @@
 "use client"
 import React, { useState } from "react"
-import "../globals.css"
+import "../app/globals.css"
 // import { Prisma } from "@prisma/client"
 import { prisma } from "../app/Database"
+import InsertNewAccountInformation from "@/app/InsertDataToDatabase"
 // import { authenticate } from '@/app/lib/actions'
 
 export default function CreateAccount() 
@@ -12,22 +13,20 @@ export default function CreateAccount()
   const [UsernameData,setUsernameData] = useState<string>("")
   const [PasswordData,setPasswordData] = useState<string>("")
 
-//  const CallDatabase = prisma.userModel.
-
   return (
    <>
    <div className="flex justify-center my-11">
-        <form>
-            <input type="text" placeholder="Jenis kelamin" value={GenderData} onChange={e => setGenderData(e.target.value)} required/>
+        <form action={InsertNewAccountInformation}>
+            <input type="text" placeholder="Jenis kelamin" name="Gender" value={GenderData} onChange={e => setGenderData(e.target.value)} required/>
             <br />
             <br />
-            <input type="text" placeholder="Email" value={EmailData} onChange={e => setEmailData(e.target.value)} required/>
+            <input type="text" placeholder="Email" name="Email" value={EmailData} onChange={e => setEmailData(e.target.value)} required/>
             <br />
             <br />
-            <input type="text" placeholder="Username" value={UsernameData} onChange={e => setUsernameData(e.target.value)} required/>
+            <input type="text" placeholder="Username" name="Username" value={UsernameData} onChange={e => setUsernameData(e.target.value)} required/>
             <br />
             <br />
-            <input type="text" placeholder="Password" value={PasswordData} onChange={e => setPasswordData(e.target.value)} required/>
+            <input type="text" placeholder="Password" name="Password" value={PasswordData} onChange={e => setPasswordData(e.target.value)} required/>
             <br />
             <br />
             <button type="submit">Simpan</button>
--- src/app/InsertDataToDatabase.ts
+++ src/app/InsertDataToDatabase.ts
@@ -1,5 +1,26 @@
+"use server"
+// import CreateAccount from "@/Components/CreateAccount"
+import CreateAccountData from "./ClientApi/CreateAccountData"
+import { prisma } from "./Database"
+
 export default async function InsertNewAccountInformation(data: FormData)
-{
-  "use-server"
-  console.log("Halo")
+{   
+  const GenderData = data.get("Gender")
+  const EmailData = data.get("Email")
+  const UsernameData = data.get("Username")
+  const PasswordData = data.get("Email")
+  console.log(GenderData)
+
+  if(typeof GenderData !== "string" || GenderData.length === 0)
+  {
+    throw new Error("Invalid")  
+  }
+  await prisma.userModel.create({
+    data:{ 
+        Genders: GenderData,
+        Email: EmailData,
+        Username: UsernameData,
+        Password: PasswordData
+    },
+  })
 }
