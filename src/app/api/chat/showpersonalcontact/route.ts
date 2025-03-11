a069de655a98f35541bfb0aee17879cf8993b470
Erza0838 <muhammaderza884@gmail.com>
2025-03-11 16:34:06 +0700

    Fitur chat bagian-1

--- src/app/api/chat/showpersonalcontact/route.ts
+++ src/app/api/chat/showpersonalcontact/route.ts
@@ -4,6 +4,12 @@ import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/auth"
 
 export const GET = async (request: NextRequest, response: NextResponse) => 
-{
-       
+{   
+    const session = await getServerSession(authOptions)
+    const ShowMyFriends = await prisma.user_Contacts.findUnique({
+        where: 
+        {
+            MyId: session?.user.id ?? ""         
+        },
+    })
 }
